b3288d852005b027daedb74d07be60048f3265fa
canyon <gongyanghui1986@163.com>
2018-03-23 17:14:14 +0800

    首次导入

---
+++ src/com/dkm/hjdl/HJDLStatistics.java
@@ -0,0 +1,288 @@
+package com.dkm.hjdl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+/**
+ * 类名 : 数据统计 打点 <br/>
+ * 作者 : canyon / 龚阳辉 <br/>
+ * 时间 : 2018-01-10 14：37 <br/>
+ * 功能 :
+ */
+public class HJDLStatistics {
+	static final public String URL = "http://z.info.cd9130.com:8122";
+	static final String gid = "22";
+	
+	// 添加 线程池
+	static ScheduledExecutorService m_excutor = Executors.newScheduledThreadPool(2);
+
+	static public String client_ver = "1.0";
+	static String uuid = "";
+	static String os = "1";
+	public String is_first = "0";
+	static String tracertid = "";
+	static int thr_index = 0;
+
+	public String appid = "";
+	public String eventtype = "";
+	public String model = "";
+	public String mac = "";
+	public String simoperator = "";
+	public String network = "";
+	public String ip = "";
+	public String imei = "";
+	public String step = "";
+	public String time_c = "";
+	public String ext = "";
+
+	private boolean isInit = false;
+	protected boolean isUUIDRnd = false;
+
+	protected boolean isLogger = true;
+	protected String logHead = "HJDLStatistics";
+
+	protected void logInfo(String msg) {
+		if (!isLogger || msg == null || msg.length() <= 0)
+			return;
+
+		System.out.println(String.format("== %s ==,msg = [%s]", logHead, msg));
+	}
+
+	public HJDLStatistics Init(String eventtype, String step, String extJson) {
+		getUUID();
+		client_ver = SDKPlgDKM.getVersionName();
+		this.appid = SDKPlgDKM.getPackageName();
+		this.model = SDKPlgDKM.getModel();
+		this.mac = SDKPlgDKM.getMacAddress();
+		this.simoperator = SDKPlgDKM.getSimOperator();
+		this.network = SDKPlgDKM.getNetWorkStatus();
+		this.ip = SDKPlgDKM.getIPAddress();
+		this.imei = SDKPlgDKM.getIMEI();
+		long sec = System.currentTimeMillis() / 1000;
+		this.time_c = String.valueOf(sec);
+
+		this.step = step;
+		this.eventtype = eventtype;
+		this.ext = extJson;
+
+		this.isInit = true;
+		// logInfo(this.toString());
+		return this;
+	}
+
+	public HJDLStatistics Init(int ntype, int nstep) {
+		return Init(String.valueOf(ntype), String.valueOf(nstep), "");
+	}
+
+	String getUUID() {
+		this.is_first = "0";
+		if (uuid == null || "".equals(uuid)) {
+			String _tmp = "";
+			String _fdir = "";
+			long lnow = System.currentTimeMillis();
+			
+			try {
+				_fdir = SDKPlgDKM.getDiskFileDir();
+				_tmp = String.format("%s%s", _fdir, "hjdl_uuid");
+				logInfo("绝对地址:" + _tmp);
+				File file = new File(_tmp);
+				if (file.exists()) {
+					FileInputStream inStream = new FileInputStream(file);
+					int size = inStream.available();
+					byte[] buf = new byte[size];
+					inStream.read(buf);
+					inStream.close();
+					uuid = new String(buf, "UTF-8");
+				} else {
+					if (isUUIDRnd)
+						_tmp = UUID.randomUUID().toString();
+					else
+						_tmp = SDKPlgDKM.getUUID();
+
+					_tmp = String.format("%s_%s", _tmp, lnow);
+					uuid = com.bowlong.security.MD5.MD5Encode(_tmp);
+
+					this.is_first = "1";
+					FileOutputStream outStrem = new FileOutputStream(file);
+					outStrem.write(uuid.getBytes("UTF-8"));
+					outStrem.flush();
+					outStrem.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (uuid != null && !"".equals(uuid)) {
+					_tmp = String.format("%s_%s", uuid, lnow);
+					tracertid = com.bowlong.security.MD5.MD5Encode(_tmp);
+				}
+			}
+		}
+		return uuid;
+	}
+
+	private List<BasicNameValuePair> getList() {
+		List<BasicNameValuePair> ret = new ArrayList<BasicNameValuePair>();
+		ret.add(new BasicNameValuePair("tracertid", tracertid));
+		ret.add(new BasicNameValuePair("eventtype", eventtype));
+		ret.add(new BasicNameValuePair("model", model));
+		ret.add(new BasicNameValuePair("mac", mac));
+		ret.add(new BasicNameValuePair("os", os));
+		ret.add(new BasicNameValuePair("simoperator", simoperator));
+		ret.add(new BasicNameValuePair("network", network));
+		ret.add(new BasicNameValuePair("ip", ip));
+		ret.add(new BasicNameValuePair("imei", imei));
+		ret.add(new BasicNameValuePair("uuid", uuid));
+		ret.add(new BasicNameValuePair("is_first", is_first));
+		ret.add(new BasicNameValuePair("gid", gid));
+		ret.add(new BasicNameValuePair("appid", appid));
+		ret.add(new BasicNameValuePair("client_ver", client_ver));
+		ret.add(new BasicNameValuePair("step", step));
+		ret.add(new BasicNameValuePair("time_c", time_c));
+		ret.add(new BasicNameValuePair("ext", ext));
+		return ret;
+	}
+
+	public void SendKVEntity() {
+		try {
+			SendEntity(new UrlEncodedFormEntity(getList(), "utf-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String getJson() {
+		try {
+			JSONObject ret = new JSONObject();
+			ret.put("tracertid", tracertid);
+			ret.put("eventtype", eventtype);
+			ret.put("model", model);
+			ret.put("mac", mac);
+			ret.put("os", os);
+			ret.put("simoperator", simoperator);
+			ret.put("network", network);
+			ret.put("ip", ip);
+			ret.put("imei", imei);
+			ret.put("uuid", uuid);
+			ret.put("is_first", is_first);
+			ret.put("gid", gid);
+			ret.put("appid", appid);
+			ret.put("client_ver", client_ver);
+			ret.put("step", step);
+			ret.put("time_c", time_c);
+
+			if (ext != null && !"".equals(ext)) {
+				ret.put("ext", new JSONObject(ext));
+			} else {
+				ret.put("ext", new JSONObject());
+			}
+			return ret.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "{}";
+	}
+
+	public void SendJson() {
+		try {
+			SendEntity(new StringEntity(getJson(), "utf-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// conTimeOut 请求超时, soTimeOut 读取超时
+	DefaultHttpClient getHttpClient(int conTimeOut,int soTimeOut){  
+        BasicHttpParams httpParams = new BasicHttpParams();  
+        HttpConnectionParams.setConnectionTimeout(httpParams, conTimeOut);  
+        HttpConnectionParams.setSoTimeout(httpParams, soTimeOut);
+        return new DefaultHttpClient(httpParams);
+    }
+
+	protected void SendEntity(HttpEntity entity) {
+		try {
+			DefaultHttpClient m_client = getHttpClient(10, 5);
+			HttpPost m_httpPost = new HttpPost(URL);
+			m_httpPost.setEntity(entity);
+			HttpResponse httpResponse = m_client.execute(m_httpPost);
+
+			String strResult = EntityUtils.toString(httpResponse.getEntity());
+			if (httpResponse.getStatusLine().getStatusCode() != 200) {
+				logInfo("Fails," + strResult);
+			} else {
+				logInfo("Successes," + strResult);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void DoStatistices() {
+		if (!this.isInit) {
+			logInfo("还没初始化!");
+			return;
+		}
+
+		if (URL == null || "".equals(URL)) {
+			logInfo("URL 为空!");
+			return;
+		}
+
+		if (tracertid == null || "".equals(tracertid)) {
+			logInfo("tracertid 为空!");
+			return;
+		}
+		
+		m_excutor.execute(new Runnable() {
+			@Override
+			public void run() {
+				SendJson();
+			}
+		});
+	}
+
+	@Override
+	public String toString() {
+		return "HJDLStatistics [is_first=" + is_first + ", isInit=" + isInit
+				+ ", appid=" + appid + ", eventtype=" + eventtype + ", model="
+				+ model + ", mac=" + mac + ", simoperator=" + simoperator
+				+ ", network=" + network + ", ip=" + ip + ", imei=" + imei
+				+ ", step=" + step + ", time_c=" + time_c + ", ext=" + ext
+				+ ", uuid=" + uuid + ", client_ver=" + client_ver
+				+ ", tracertid=" + tracertid + "]";
+	}
+
+	public static void main(String[] args) {
+		long lnow = System.currentTimeMillis();
+		String _tmp, _md5;
+		_tmp = String.format("%s_%s", UUID.randomUUID().toString(), lnow);
+		_md5 = com.bowlong.security.MD5.MD5Encode(_tmp);
+		System.out.println(_md5);
+
+		_tmp = String.format("%s_%s", _md5, lnow);
+		_md5 = com.bowlong.security.MD5.MD5Encode(_tmp);
+		System.out.println(_md5);
+
+		HJDLStatistics ret = new HJDLStatistics();
+		ret.isUUIDRnd = true;
+		System.out.println(ret.getJson());
+		System.out.println(ret.getUUID());
+	}
+}
---
+++ src/com/dkm/hjdl/SDKPlgDKM.java
@@ -0,0 +1,340 @@
+package com.dkm.hjdl;
+
+import org.json.JSONObject;
+
+import cc.jyslproxy.framework.bean.JyslPayParam;
+import cc.jyslproxy.framework.bean.JyslRoleParam;
+import cc.jyslproxy.framework.callback.JyslResultCallback;
+import cc.jyslproxy.openapi.JyslSDK;
+
+import com.sdkplugin.bridge.U3DBridge;
+import com.sdkplugin.extend.PluginBasic;
+import com.sdkplugin.tools.Tools;
+
+/**
+ * 类名 : 哆可梦 渠道的SDK <br/>
+ * 作者 : canyon / 龚阳辉 <br/>
+ * 时间 : 2018-01-02 17：01 <br/>
+ * 功能 : 实现登录,切换帐号,注销,支付等,也是实现了消息监听
+ */
+public class SDKPlgDKM extends PluginBasic implements JyslResultCallback {
+	static final String CMD_DKM_Init = "/dkm/init";
+	static final String CMD_DKM_ChangeUser = "/dkm/changeUser";
+
+	static final String CMD_DKM_InitRinfo = "/dkm/initRInfo";
+	static final String CMD_DKM_ReRname = "/dkm/reRname";
+	static final String CMD_DKM_Login = "/dkm/login";
+	static final String CMD_DKM_Logout = "/dkm/logout";
+	static final String CMD_DKM_Pay = "/dkm/pay";
+	static final String CMD_DKM_ExitGame = "/dkm/exitGame";
+
+	static final String CMD_DKM_RCreate = "/dkm/rCreate";
+	static final String CMD_DKM_REntryGame = "/dkm/rEntryGame";
+	static final String CMD_DKM_RUpLv = "/dkm/rUpLv";
+
+	// 取得登录后的用户信息
+	static final String CMD_DKM_GetUser = "/dkm/getUser";
+
+	// 数据统计
+	static final String CMD_DKM_Statistics = "/dkm/statistics";
+
+	boolean isInitSuccess = false;
+
+	String gameId = "", partnerId = "", sdk = "";
+
+	String userid = "", account = "", token = "";
+
+	// 角色相关信息
+	String rid = "", rname = "", svid = "", svname = "",
+			createtime = "1456397360";
+
+	int rlv = 0;
+
+	enum RinfoState {
+		Create, EntryGame, UpLv
+	}
+
+	public SDKPlgDKM() {
+		super();
+		this.logLevel = LEV_LOG_NORMAL;
+		this.logHead = "dkm";
+	}
+
+	@Override
+	public void receiveFromUnity(String json) throws Exception {
+		if (json == null || json.length() <= 0) {
+			logMust("json is null or empty");
+			return;
+		}
+		logInfo(json);
+
+		JSONObject obj = new JSONObject(json);
+		final String cmd = obj.getString("cmd");
+		handlerMsg(cmd, obj);
+	}
+
+	void UserInfo(String code) throws Exception {
+		JSONObject data = new JSONObject();
+		data.put("gameId", gameId);
+		data.put("partnerId", partnerId);
+		data.put("sdk", sdk);
+		data.put("token", token);
+		data.put("userid", userid);
+
+		data.put("imei", getIMEI());
+		data.put("device", 1);
+		data.put("ifda", "");
+
+		if (code != null && !"".equals(code)) {
+			data.put("cmd", code);
+			Tools.msg2U3D(CODE_SUCCESS, "", data);
+
+			data.remove("cmd");
+		}
+
+		data.put("cmd", CMD_DKM_GetUser);
+		Tools.msg2U3D(CODE_SUCCESS, "", data);
+	}
+
+	@Override
+	public void onResult(int code, JSONObject data) {
+		try {
+			switch (code) {
+			case JyslResultCallback.CODE_INIT_SUCCESS:
+				gameId = data.getString("game_id");
+				// partner_id 对应映射平台关系请参考对接文档中平台号对应表格
+				partnerId = data.getString("partner_id");
+				sdk = JyslSDK.getInstance().getSdkPartnerid();
+
+				logInfo("初始化成功");
+				isInitSuccess = true;
+				Tools.msg2U3D(CODE_SUCCESS, "", Tools.ToData(CMD_DKM_Init, ""));
+				new HJDLStatistics().Init(2, 1).DoStatistices();
+				break;
+			case JyslResultCallback.CODE_INIT_FAILURE:
+				logMust("初始化失败,需再次调用初始化方法");
+				Tools.msg2U3D(CODE_FAILS, "初始化失败,会再次调用初始化方法的",
+						Tools.ToData(CMD_DKM_Init, ""));
+				JyslSDK.getInstance().init(getCurActivity());
+				break;
+			case JyslResultCallback.CODE_LOGIN_SUCCESS:
+				logInfo("登录成功");
+				userid = data.getString("userid");
+				account = data.getString("account");
+				token = data.getString("token");
+
+				UserInfo(CMD_DKM_Login);
+				break;
+			case JyslResultCallback.CODE_LOGIN_FAILURE:
+				logMust("登录失败");
+				Tools.msg2U3D(CODE_FAILS, "登录失败",
+						Tools.ToData(CMD_DKM_Login, ""));
+				break;
+			case JyslResultCallback.CODE_SWITCH_ACCOUNT_SUCCESS:
+				logInfo("切换帐号成功");
+				userid = data.getString("userid");
+				account = data.getString("account");
+				token = data.getString("token");
+
+				UserInfo(CMD_DKM_ChangeUser);
+				break;
+			case JyslResultCallback.CODE_SWITCH_ACCOUNT_FAILURE:
+				logMust("切换帐号失败");
+				Tools.msg2U3D(CODE_FAILS, "切换帐号失败",
+						Tools.ToData(CMD_DKM_ChangeUser, ""));
+				break;
+			case JyslResultCallback.CODE_LOGOUT_SUCCESS:
+				logInfo("注销成功");
+				userid = "";
+				account = "";
+				token = "";
+				Tools.msg2U3D(CODE_SUCCESS, "",
+						Tools.ToData(CMD_DKM_Logout, ""));
+				break;
+			case JyslResultCallback.CODE_LOGOUT_FAILURE:
+				logInfo("注销失败");
+				Tools.msg2U3D(CODE_FAILS, "", Tools.ToData(CMD_DKM_Logout, ""));
+				break;
+			case JyslResultCallback.CODE_PAY_SUCCESS:
+				// 支付成功 ， 这个成功并不一定是成功，有的平台是异步的，只是订单流程走通
+				logInfo("支付流程成功，请等待服务器发送资源!");
+				Tools.msg2U3D(CODE_SUCCESS, "支付流程成功，请等待服务器发资源!",
+						Tools.ToData(CMD_DKM_Pay, ""));
+				break;
+			case JyslResultCallback.CODE_PAY_WAIT:
+				logInfo("已支付，等待确认");
+				Tools.msg2U3D(CODE_WAIT, "已支付，等待确认",
+						Tools.ToData(CMD_DKM_Pay, ""));
+				break;
+			case JyslResultCallback.CODE_PAY_FAILURE:
+				logMust("支付失败");
+				Tools.msg2U3D(CODE_FAILS, "支付失败", Tools.ToData(CMD_DKM_Pay, ""));
+				break;
+			case JyslResultCallback.CODE_PAY_CANCEL:
+				logMust("支付取消");
+				Tools.msg2U3D(CODE_FAILS, "支付取消", Tools.ToData(CMD_DKM_Pay, ""));
+				break;
+			case JyslResultCallback.EXITGAME:
+				logMust("处理游戏关闭逻辑");
+				// 第三方渠道有自己退出界面的时候，当点击第三方渠道的确定退出按钮时候的回调,此时在处理自身的逻辑
+				MainActivity.sendMsg(0);
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	boolean isInitSuccessed(String extDesc) {
+		if (!isInitSuccess)
+			System.out.println(String.format("初始化未完成，请稍后再次尝试登陆(%s)", extDesc));
+
+		return isInitSuccess;
+	}
+
+	void login() {
+		if (!isInitSuccessed("login")) {
+			return;
+		}
+		JyslSDK.getInstance().login();
+	}
+
+	void logout() {
+		if (!isInitSuccessed("logout")) {
+			return;
+		}
+		JyslSDK.getInstance().logout();
+	}
+
+	void updateRoleInfo(int lv, RinfoState emState) {
+		this.rlv = lv;
+
+		if (!isInitSuccessed("updateRoleInfo")) {
+			return;
+		}
+
+		JyslRoleParam roleParam = new JyslRoleParam();
+		roleParam.setRoleId(rid);// 角色ID，字符串类型
+		roleParam.setRoleName(rname);// 角色名字，字符串类型
+		roleParam.setRoleLevel(rlv);// 角色等级，int类型
+		roleParam.setServerId(svid);// 服务器ID，字符串类型
+		roleParam.setServerName(svname); // 服务器名字，字符串类型
+		// 获取服务器存储的角色创建时间,时间戳，单位秒，长度10，不可用本地手机时间
+		// ，同一角色创建时间不可变，上线UC联运必需接入，（字符串类型，sdk内如会有转换）
+		roleParam.setRoleCreateTime(createtime);
+		switch (emState) {
+		case Create:
+			JyslSDK.getInstance().createRole(roleParam);
+			break;
+		case UpLv:
+			JyslSDK.getInstance().roleUpLevel(roleParam);
+			break;
+		default:
+			JyslSDK.getInstance().enterGame(roleParam);
+			break;
+		}
+	}
+
+	void InitRinfo(String rid, String rname, String svid, String svname,
+			String createtime) {
+		this.rid = rid;
+		ReRname(rname);
+		this.svid = svid;
+		this.svname = svname;
+		this.createtime = createtime;
+	}
+
+	void ReRname(String rname) {
+		this.rname = rname;
+	}
+
+	// 支付 price 单位为分
+	void pay(String cpOrderId, String pdId, String pdName, String pdDesc,
+			float price, String ext) {
+
+		if (!isInitSuccessed("pay")) {
+			return;
+		}
+
+		JyslPayParam payParam = new JyslPayParam();
+		payParam.setCpBill(cpOrderId); // cp（游戏方）订单，字符串类型
+		payParam.setProductId(pdId); // 商品标识 ，字符串类型
+		payParam.setProductName(pdName); // 商品名称，字符串类型
+		payParam.setProductDesc(pdDesc); // 商品说明，字符串类型
+		payParam.setServerId(svid); // 服务器编号，字符串类型
+		payParam.setServerName(svname);// 服务器名称
+		payParam.setRoleId(rid); // 角色id，字符串类型
+		payParam.setRoleName(rname); // 角色名，字符串类型
+		payParam.setRoleLevel(rlv); // 角色等级（int类型）
+		payParam.setPrice(price / 100); // 价格(单位元)(float 类型)
+		payParam.setExtension(ext);// 会原样返回给游戏，字符串类型
+		JyslSDK.getInstance().pay(payParam);
+	}
+
+	public void handlerMsg(final String code, JSONObject data) throws Exception {
+		switch (code) {
+		case CMD_DKM_Init:
+			if (isInitSuccess) {
+				Tools.msg2U3D(CODE_SUCCESS, "", Tools.ToData(CMD_DKM_Init, ""));
+			}
+			break;
+		case CMD_DKM_InitRinfo:
+			InitRinfo(data.getString("rid"), data.getString("rname"),
+					data.getString("svid"), data.getString("svname"),
+					data.getString("createtime"));
+			break;
+		case CMD_DKM_ReRname:
+			ReRname(data.getString("rname"));
+			break;
+		case CMD_DKM_Login:
+			login();
+			break;
+		case CMD_DKM_Logout:
+			logout();
+			break;
+		case CMD_DKM_Pay:
+			pay(data.getString("orid"), data.getString("pdid"),
+					data.getString("pdname"), data.getString("pddesc"),
+					data.getInt("price"), data.getString("ext"));
+			break;
+		case CMD_DKM_ExitGame:
+			MainActivity.sendMsg(0);
+			break;
+		case CMD_DKM_RCreate:
+			// 创建角色
+			updateRoleInfo(data.getInt("lv"), RinfoState.Create);
+			break;
+		case CMD_DKM_REntryGame:
+			// 进入游戏
+			updateRoleInfo(data.getInt("lv"), RinfoState.EntryGame);
+			break;
+		case CMD_DKM_RUpLv:
+			// 角色升级
+			updateRoleInfo(data.getInt("lv"), RinfoState.UpLv);
+			break;
+		case CMD_DKM_GetUser:
+			UserInfo("");
+			break;
+		case CMD_DKM_Statistics:
+			HJDLStatistics oneStatistics = new HJDLStatistics();
+			oneStatistics.Init(data.getString("ntype"),
+					data.getString("nstep"), "");
+			oneStatistics.DoStatistices();
+			break;
+		default:
+			handlerJson(data);
+			break;
+		}
+	}
+
+	static SDKPlgDKM _instance;
+
+	static public SDKPlgDKM getInstance() {
+		if (_instance == null) {
+			_instance = new SDKPlgDKM();
+
+			U3DBridge.setListener(_instance);
+		}
+		return _instance;
+	}
+}

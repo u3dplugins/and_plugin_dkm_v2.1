5b7d671f5e30928a56d42206779a89358853b753
canyon <gongyanghui1986@163.com>
2019-07-13 17:16:24 +0800

    修改版本为26，做6.0的动态权限申请

--- src/com/dkm/hjdl/MainActivity.java
+++ src/com/dkm/hjdl/MainActivity.java
@@ -2,6 +2,7 @@ package com.dkm.hjdl;
 
 import com.dcproxy.framework.callback.IShowLogoCallBack;
 import com.dcproxy.openapi.JyslSDK;
+import com.sdkplugin.bridge.AndPermission;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import android.app.AlertDialog;
@@ -25,6 +26,7 @@ import android.view.KeyEvent;
  */
 public class MainActivity extends com.unity3d.player.UnityPlayerActivity {
 	TelephonyManager m_mgrTM = null;
+
 	@Override
 	public void onConfigurationChanged(Configuration arg0) {
 		super.onConfigurationChanged(arg0);
@@ -33,35 +35,36 @@ public class MainActivity extends com.unity3d.player.UnityPlayerActivity {
 
 	@Override
 	protected void onCreate(Bundle bundle) {
+		AndPermission.initPermissions(this);
 		super.onCreate(bundle);
 		_bug4U56Fragment();
-		
+
 		this.m_mgrTM = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-		
-		//横屏
+
+		// 横屏
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		
+
 		// 初始化bugly的异常捕获 b336483791 61e3febef5 JyslSDK里有监听崩溃日志
-		CrashReport.initCrashReport(getApplicationContext(), "61e3febef5",false);
-		
+		CrashReport.initCrashReport(getApplicationContext(), "61e3febef5", false);
+
 		// 数据统计
 		new HJDLStatistics().Init(1, 0).DoStatistices();
 
 		initMHandler();
-		
+
 		// 初始化，必须调用
 		JyslSDK.getInstance().setResultCallback(SDKPlgDKM.getInstance());
 		new HJDLStatistics().Init(2, 0).DoStatistices();
 		JyslSDK.getInstance().init(MainActivity.this);
 		JyslSDK.getInstance().onCreate(bundle);
-		//_showSplash(3000);
+		// _showSplash(3000);
 
 		// 更新信息收集接口和点击进入游戏按钮事件收集（可选接入）
 		// AkRoleParam param = new AkRoleParam();
 		// AkRoleParam 如果有角色信息则填入AkRoleParam 没有则不用操作
 		// JyslSDK.getInstance().CheckUpdateStart(param); // 游戏检查更新开始信息收集
 		// JyslSDK.getInstance().CheckUpdateFinish(param); // 游戏检查更新完成信息收集
-		// JyslSDK.getInstance().ClickEnterGameButton(param); // 点击游戏开始按钮信息收集		
+		// JyslSDK.getInstance().ClickEnterGameButton(param); // 点击游戏开始按钮信息收集
 	}
 
 	@Override
@@ -74,14 +77,14 @@ public class MainActivity extends com.unity3d.player.UnityPlayerActivity {
 	protected void onPause() {
 		super.onPause();
 		JyslSDK.getInstance().onPause();
-		SDKPlgDKM.reListenerState(this.m_mgrTM,true);
+		SDKPlgDKM.reListenerState(this.m_mgrTM, true);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		JyslSDK.getInstance().onResume();
-		SDKPlgDKM.reListenerState(this.m_mgrTM,false);
+		SDKPlgDKM.reListenerState(this.m_mgrTM, false);
 	}
 
 	@Override
@@ -119,50 +122,55 @@ public class MainActivity extends com.unity3d.player.UnityPlayerActivity {
 		super.onStop();
 		JyslSDK.getInstance().onStop();
 	}
-	
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		AndPermission.onRequestPermissionsResult(requestCode, permissions, grantResults,this);
+	}
+
 	void _bug4U56Fragment(boolean _isFlags) {
 		getWindow().setFormat(-3);
 		this.mUnityPlayer = new CUnityPlayer(this);
-	    if (_isFlags && this.mUnityPlayer.getSettings().getBoolean("hide_status_bar", true)) {
-	      getWindow().setFlags(1024, 1024);
-	    }
-	    setContentView(this.mUnityPlayer);
-	    this.mUnityPlayer.requestFocus();
+		if (_isFlags && this.mUnityPlayer.getSettings().getBoolean("hide_status_bar", true)) {
+			getWindow().setFlags(1024, 1024);
+		}
+		setContentView(this.mUnityPlayer);
+		this.mUnityPlayer.requestFocus();
 	}
-	
+
 	void _bug4U56Fragment() {
 		_bug4U56Fragment(true);
 	}
 
 	boolean _KeyCodeEvent(int keyCode, KeyEvent event) {
-		//拦截返回键
-        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK){
-            //判断触摸UP事件才会进行返回事件处理 KeyEvent.ACTION_UP
-            if (event.getAction() == KeyEvent.ACTION_DOWN) {
-                onBackPressed();
-                //只要是返回事件，直接返回true，表示消费掉
-                return true;
-            }
-        }
-        return false;
+		// 拦截返回键
+		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+			// 判断触摸UP事件才会进行返回事件处理 KeyEvent.ACTION_UP
+			if (event.getAction() == KeyEvent.ACTION_DOWN) {
+				onBackPressed();
+				// 只要是返回事件，直接返回true，表示消费掉
+				return true;
+			}
+		}
+		return false;
 	}
-	
-//	@Override
-//	public boolean dispatchKeyEvent(KeyEvent event) {
-//		if(_KeyCodeEvent(event.getKeyCode(), event)){
-//			return true;
-//		}
-//		return super.dispatchKeyEvent(event);
-//	}
-	
+
+	// @Override
+	// public boolean dispatchKeyEvent(KeyEvent event) {
+	// if(_KeyCodeEvent(event.getKeyCode(), event)){
+	// return true;
+	// }
+	// return super.dispatchKeyEvent(event);
+	// }
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if(_KeyCodeEvent(keyCode, event)){
+		if (_KeyCodeEvent(keyCode, event)) {
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 		// 游戏方负责关闭游戏,通过JyslResultCallback.EXITGAME 回调，没有这需求可以不用管
@@ -175,20 +183,17 @@ public class MainActivity extends com.unity3d.player.UnityPlayerActivity {
 	}
 
 	void showTip4Exit() {
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this)
-				.setTitle("退出游戏").setMessage("您确定要退出游戏么?")
-				.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						exitGame();
-					}
-				})
-				.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				});
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this).setTitle("退出游戏").setMessage("您确定要退出游戏么?").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				exitGame();
+			}
+		}).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
 		alertDialog.show();
 	}
 
@@ -215,15 +220,15 @@ public class MainActivity extends com.unity3d.player.UnityPlayerActivity {
 			};
 		}
 	}
-	
-	void _showSplash(int ms){
+
+	void _showSplash(int ms) {
 		// 会读取 assets 目录下的名为 splash 的 png 格式的图片
-		JyslSDK.getInstance().showLogo(this,"splash.png",ms,new IShowLogoCallBack() {
-            @Override
-            public void onFinished(String msg, int code) {
-                   //显示闪屏结束
-            }
-        });
+		JyslSDK.getInstance().showLogo(this, "splash.png", ms, new IShowLogoCallBack() {
+			@Override
+			public void onFinished(String msg, int code) {
+				// 显示闪屏结束
+			}
+		});
 	}
 
 	static Handler mHandler = null;

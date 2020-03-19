cddd5aff0bf285e94cd2cc51cfd30c9f5955191f
canyon <gongyanghui1986@163.com>
2020-03-19 17:59:50 +0800

    添加权限

---
+++ src/com/dkm/hjdl/SDKPlgPermission.java
@@ -0,0 +1,16 @@
+package com.dkm.hjdl;
+
+import android.Manifest;
+import android.app.Activity;
+
+public class SDKPlgPermission {
+	static private String[] _perms = { 
+			Manifest.permission.READ_PHONE_STATE, 
+			Manifest.permission.WRITE_EXTERNAL_STORAGE, 
+			Manifest.permission.READ_EXTERNAL_STORAGE, 
+			Manifest.permission.RECORD_AUDIO };
+
+	static public void reqPermission(Activity cur) {
+		SDKPlgDKM.initPermissions(cur, _perms);
+	}
+}

d5aef5f105b3fa23992cbbcb5d802662ac3c1c61
canyon <gongyanghui1986@163.com>
2020-03-19 16:29:04 +0800

    调整权限申请

--- reback/SDKPlgPermission.java
+++ reback/SDKPlgPermission.java
@@ -4,7 +4,11 @@ import android.Manifest;
 import android.app.Activity;
 
 public class SDKPlgPermission {
-	static private String[] _perms = { Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO };
+	static private String[] _perms = { 
+			Manifest.permission.READ_PHONE_STATE, 
+			Manifest.permission.WRITE_EXTERNAL_STORAGE, 
+			Manifest.permission.READ_EXTERNAL_STORAGE, 
+			Manifest.permission.RECORD_AUDIO };
 
 	static public void reqPermission(Activity cur) {
 		SDKPlgDKM.initPermissions(cur, _perms);
